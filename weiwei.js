0f0e69c3e3601139cb812d90a6d89e2c24ec4cfa
unknown <liuln@cytsonline.com>
2018-07-16 12:12:50 +0800

    Signed-off-by: unknown <liuln@cytsonline.com>

--- weiwei.js
+++ weiwei.js
@@ -3,7 +3,16 @@ var app=express();
 
 //设置handlebars视图引擎
 var handlebars=require('express3-handlebars')
-	.create({defaultLayout:'main'});
+	.create({
+		defaultLayout:'main',
+		helper:{
+			section:function (name,options) {
+				if(!this._sections) this._sections={};
+				this._sections[name]=options.fn(this);
+				return null;
+			}
+		}
+	});
 app.engine('handlebars',handlebars.engine);
 app.set('view engine','handlebars');
 
