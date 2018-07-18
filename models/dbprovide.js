9ebd16377087e2e18ac19ea3951557a07a836928
unknown <liuln@cytsonline.com>
2018-07-18 14:26:35 +0800

    Signed-off-by: unknown <liuln@cytsonline.com>

--- models/dbprovide.js
+++ models/dbprovide.js
@@ -1,24 +1,16 @@
-mysqlConfig:{
-	production:{
-		host :'39.107.252.17',
-		user :'root',
-		password :'lina2010',
-		port :'3306',
-		database :'WeiWeiStock'
-	}
-};
-var mysql=require('mysql');
-var connection=mysql.createConnection(mysqlConfig);
+
+var connection = mysql.createConnection({
+  host     : '39.107.252.17',
+  user     : 'root',
+  password : 'lina2010',
+  database : 'WeiWeiStock'
+});
+
 connection.connect();
 
-var sql='SELECT * from WeiManager';
-
-connection.query(sql,function (err,result) {
-	if(err){
-		console.log('查询报错-',err.message);
-		return;
-	}
-   console.log('--------------------------SELECT----------------------------');
-   console.log(result);
-   console.log('------------------------------------------------------------\n\n');  
+connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results[0].solution);
 });
+
+connection.end();
