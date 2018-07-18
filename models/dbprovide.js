dc6119b66dba9409dcac28159abbe4228be00b6b
unknown <liuln@cytsonline.com>
2018-07-18 15:12:17 +0800

    Signed-off-by: unknown <liuln@cytsonline.com>

--- models/dbprovide.js
+++ models/dbprovide.js
@@ -1,18 +1,2 @@
 var mysql      = require('mysql');
   console.log('mysql');
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'lina2010',
-  database : 'WeiWeiStock'
-});
-
-  console.log('connection');
-connection.connect();
-
-connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-  if (error) throw error;
-  console.log('The solution is: ', results[0].solution);
-});
-
-connection.end();
