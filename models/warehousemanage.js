aa8c1f71afecac4e898a600b5c07566a76b20b0f
unknown <liuln@cytsonline.com>
2018-07-19 15:03:21 +0800

    Signed-off-by: unknown <liuln@cytsonline.com>

--- models/warehousemanage.js
+++ models/warehousemanage.js
@@ -37,7 +37,7 @@ router.get('/getwarehouselist2',function (req,res) {
 			$.each(rows,function (i,obj) {
 				formateRows.push({id:obj.ID,text:obj.Name});
 			});
-			res.json({results:formateRows});
+			res.json({"results":formateRows});
 		}
 	});
 });
