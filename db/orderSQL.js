0eff3f06a50bd924bd1f6cf2da3f605c90e57a5c
unknown <liuln@cytsonline.com>
2018-08-08 09:42:16 +0800

    Signed-off-by: unknown <liuln@cytsonline.com>

--- db/orderSQL.js
+++ db/orderSQL.js
@@ -1,4 +1,4 @@
 var orderSQL={
-	add:'insert into WeiOrder ( `ProductID`, `OrderNO`,`Price`,`UserID`,`IsBalance`,`InputTime`,`TotalPrice`,`Number`,`IsPack`,`IsPost`,`PostCode`,`BalanceTime`,`PackTime`,`PostTime`,`UserWeiXinID`）values（?,?,?,?,0,?,?,?,0,0,'',null,null,null,?)'
+	add:'insert into WeiOrder(ProductID, OrderNO,Price,UserID,IsBalance,InputTime,TotalPrice,Number,IsPack,IsPost,PostCode,BalanceTime,PackTime,PostTime,UserWeiXinID)values(?,?,?,?,0,?,?,?,0,0,"",null,null,null,?)'
 };
 module.exports = orderSQL;
