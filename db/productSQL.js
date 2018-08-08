47d04a7fda052812e32fbfa079a814a67f74ac5c
unknown <liuln@cytsonline.com>
2018-08-08 11:18:17 +0800

    Signed-off-by: unknown <liuln@cytsonline.com>

--- db/productSQL.js
+++ db/productSQL.js
@@ -3,7 +3,7 @@ var productSQL = {
     addImages:'insert into WeiImages ( ImgUrl,InputTime,WWMID ) values (?,?,?) ',
     addProductImage:'insert into WeiProductImage (Remark,ImageID,ProductID ) values ? ',
     addImageType:'insert into WeiProductImageType(ProductID,Price,UsedNumber,TotalNumber,Status,ImageID,Remark)values ? ',
-    queryPouductAllForSelect:'select p.`ID` PID,p.`ProductName`,p.`Remark`,p.`TotalNumber`,p.`UsedNumber`,p.Width,imgType.`ID` TypeID,imgType.`ImageID`,imgType.`Price`,imgType.`Remark`,imgType.`TotalNumber`,imgType.`UsedNumber`,img.`ImgUrl` from `WeiProductImageType` imgType left join `WeiProduct` p on imgType.`ProductID`=p.`ID` left join `WeiImages` img on imgType.`ImageID`=img.`ID` where p.TotalNumber-p.UsedNumber>0 and p.`Status`=30 order by p.`ID`,imgType.`ID`',
+    queryPouductAllForSelect:'select p.`ID` PID,p.`ProductName`,p.`Remark`,p.`TotalNumber`,p.`UsedNumber`,p.Width, p.TotalNumber-p.UsedNumber p.StockNumber,imgType.`ID` TypeID,imgType.`ImageID`,imgType.`Price`,imgType.`Remark`,imgType.`TotalNumber`,imgType.`UsedNumber`,img.`ImgUrl` from `WeiProductImageType` imgType left join `WeiProduct` p on imgType.`ProductID`=p.`ID` left join `WeiImages` img on imgType.`ImageID`=img.`ID` where p.TotalNumber-p.UsedNumber>0 and p.`Status`=30 order by p.`ID`,imgType.`ID`',
     addStockList:'insert into `WeiStockList`(Number,InputTime,ProductImageTypeID,ProductID)values ?',
     queryHouseAll:'select * from WeiWarehouseType',
     minuxNumber:' update WeiProduct set TotalNumber=TotalNumber-?,UsedNumber=UsedNumber+? where ProductID=? ',
