120674ba6bd3c01438ab3aed3701ea2483716c82
liuln6 <li-na6@live.cn>
2018-08-09 22:08:15 +0800

    2

--- public/script/validate.js
+++
@@ -1,9 +0,0 @@
-Validator.extend('phone', {
-  messages: {
-    zh_CN:field => field + '必须是11位手机号码',
-  },
-  validate: value => {
-    return value.length == 11 && /^((13|14|15|17|18)[0-9]{1}\d{8})$/.test(value)
-  }
-});
-
