5d6a78b4166d7c24f9af7f5e0e10a3bdb7ef854a
liuln6 <li-na6@live.cn>
2018-08-16 16:14:19 +0800

    1

--- models/product.js
+++ models/product.js
@@ -183,6 +183,7 @@ router.get('/packinfo',function (req,res) {
 		if(err){
 			res.send('获取打包信息失败'+ err);
 		}else{
+			console.log(result);
 			var result = JSON.stringify(result[0]);
 	        var resultOrder=JSON.stringify(result[1]);
 	        result= JSON.parse(result);//把results字符串转为json对象
