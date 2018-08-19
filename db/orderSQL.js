27405c434e1c22e80abd5a3b4fda22c9e45a9f42
liuln6 <li-na6@live.cn>
2018-08-19 12:10:10 +0800

    2

--- db/orderSQL.js
+++ db/orderSQL.js
@@ -13,6 +13,8 @@ var orderSQL={
 	updateAllot:'update `WeiOrder` set `IsAllot`=?,`AllotTime`=? where `UserID`=? and ID in(?)',
 	updateBalence:'update `WeiOrder` set `IsBalance`=?,`BalanceTime`=? where `UserID`=? and ID in(?)',
 	queryPostList:'select wp.*,wu.`UserName`,wu.`WeiXinName` from WeiPack wp left join `WeiUsers` wu on wp.UserID=wu.`ID`',
-	queryPostInfo:'select ID,WeiXinID,WeiXinName,`UserName` from `WeiUsers` where ID=?; select ID,Address,Name,Phone,ZipCode from `WeiUsersAddress` where `UserID`=?; select wo.*,wp.`ProductName`,wpt.`Remark` TypeName from `WeiOrder` wo left join `WeiProduct` wp on wo.`ProductID`=wp.ID left join `WeiProductImageType` wpt on wo.`TypeID`=wpt.ID where wo.ID in( select `OrderID` from `WeiOrderPack` where `PackID`=?)'
+	queryPostInfo:'select ID,WeiXinID,WeiXinName,`UserName` from `WeiUsers` where ID=?; select ID,Address,Name,Phone,ZipCode from `WeiUsersAddress` where `UserID`=?; select wo.*,wp.`ProductName`,wpt.`Remark` TypeName from `WeiOrder` wo left join `WeiProduct` wp on wo.`ProductID`=wp.ID left join `WeiProductImageType` wpt on wo.`TypeID`=wpt.ID where wo.ID in( select `OrderID` from `WeiOrderPack` where `PackID`=?)',
+	editPost:'update WeiPack set IsPost=?,PostTime=? where ID=? ',
+	addPostInfo:'insert into WeiPostInfo (CID,CName,Code,InputTime,Price)values(?,?,?,?,?)'
 };
 module.exports = orderSQL;
