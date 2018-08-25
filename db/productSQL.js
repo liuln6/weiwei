b8e08a13d59cc396445500ac9891910e2dc3f9ea
liuln6 <li-na6@live.cn>
2018-08-25 09:49:38 +0800

    2

--- db/productSQL.js
+++ db/productSQL.js
@@ -18,6 +18,6 @@ var productSQL = {
     queryProductTypeListForPack:'select wo.*,wp.`ProductName`,wpt.`Remark` TypeName from (select `ProductID`,`TypeID`,count(1) Total,sum(`Number`) TotalNumber from `WeiOrder` where `IsPack`=0 group by `ProductID`,`TypeID` ) wo left join `WeiProduct` wp on wo.`ProductID`=wp.`ID`  left join `WeiProductImageType` wpt on wo.`TypeID`=wpt.`ID` ',
     queryTypeInfo:'select wpt.*,wp.`ProductName` from `WeiProductImageType` wpt left join `WeiProduct` wp on wpt.`ProductID`=wp.`ID` where wpt.ID=?;select * from `WeiOrder` where `TypeID`=? and OrderStatu=30 ',
     queryUserOrderInfo:'select ID,WeiXinID,WeiXinName,`UserName` from `WeiUsers` where ID=?; select ID,Address,Name,Phone,ZipCode from `WeiUsersAddress` where `UserID`=?; select wo.*,wp.`ProductName`,wpt.`Remark` TypeName from `WeiOrder` wo left join `WeiProduct` wp on wo.`ProductID`=wp.ID left join `WeiProductImageType` wpt on wo.`TypeID`=wpt.ID where `UserID`=? and `IsPack`=0 and OrderStatu=30',
-    edittype:'update WeiProductImageType set Price=?,TotalNumber=? where ID=?'
+    edittype:'update WeiProductImageType set Price=?,TotalNumber=TotalNumber+? where ID=?'
 };
 module.exports = productSQL;
